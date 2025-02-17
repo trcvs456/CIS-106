85753f087e3669fa9403a381437d5718cc2a6b8f
trcvs456 <gs48097@mail.harpercollege.edu>
2025-02-17 14:57:04 -0600

    Delete M4P4.py

--- M4P4.py
+++
@@ -1,4 +0,0 @@
-first_name=input("enter first name:")
-number_of_steps=float(input("enter the number of steps:"))
-burned_calories=number_of_steps*0.25
-print(f"{first_name:}'s total calories burned is: {burned_calories:}")
