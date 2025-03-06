d91f8187f85bc93e2ff64c1735afce728de5fa54
trcvs456 <gs48097@mail.harpercollege.edu>
2025-03-06 11:32:33 -0600

    Delete Module 7 directory

--- Module 7/M7P1.py
+++
@@ -1,9 +0,0 @@
-# Initialize the starting number
-num = 1
-
-# Use a while loop to print odd numbers up to 25
-while num <= 25:
-    print(num)
-    num += 2  # Increment by 2 to ensure only odd numbers are printed
-
-
